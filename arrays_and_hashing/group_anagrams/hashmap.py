aefc69be1b00b7b671c31f9b737b7f595f2c2588
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-01-01 19:30:54 +0100

    saved solution

---
+++ arrays_and_hashing/group_anagrams/hashmap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagram_map = {}
+        
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            if sorted_s in anagram_map:
+                anagram_map[sorted_s].append(s)
+            else:
+                anagram_map[sorted_s] = [s]
+        
+        return list(anagram_map.values())
