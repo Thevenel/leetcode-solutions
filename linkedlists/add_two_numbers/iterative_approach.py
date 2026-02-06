851366a41bdc4123878ee1825fb3919e9afa43c3
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-06 19:27:04 +0100

    add two numbers

---
+++ linkedlists/add_two_numbers/iterative_approach.py
@@ -0,0 +1,39 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+        
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
+
+# Usage example:
+# Creating two linked lists: 2 -> 4 -> 3 and 5 -> 6 -> 4
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+solution = Solution()
+result = solution.addTwoNumbers(l1, l2)
+# Now result is the linked list representing the sum: 7 -> 0 -> 8
+while result:
+    print(result.val)  # Output: 7, 0, 8
+    result = result.next
