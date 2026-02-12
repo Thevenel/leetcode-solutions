bfe0b4236fcce065e1583f017227f9affd6831fa
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-12 18:23:20 +0100

    max depth of binary tree iter dfs

---
+++ trees/maximum_depth_of_binary_tree/iterative_dfs.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+        
+        
+# Usage example:
+solution = Solution()
+# Creating a binary tree: # 3 # / \
+#              9   20 # / \
+#            15   7
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(solution.maxDepth(root))# Output: 3
