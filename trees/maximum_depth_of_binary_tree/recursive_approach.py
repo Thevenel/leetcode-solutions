ca092f7a8efb9504a1cbe21d2ce17315f6093844
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-12 17:47:15 +0100

    max depth of binary tree rec

---
+++ trees/maximum_depth_of_binary_tree/recursive_approach.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+# Usage example:
+solution = Solution()
+# Creating a binary tree: # 3 # / \
+#              9   20 # / \
+#            15   7
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(solution.maxDepth(root))# Output: 3
