1a233b4d553d1152d32708ca4b1c15bce540b8a6
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-17 18:01:42 +0100

    lca

---
+++ trees/lowest_common_ancestor/interative_approach.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        cur = root
+        while cur:
+            if p.val > root.val and q.val > root.val:
+                cur = cur.right
+            elif p.val < root.val and q.val < root.val:
+                cur = cur.left
+            else:
+                return cur
+
+# Usage
+# Constructing the binary tree
+#         6
+#        / \
+#       2   8
+#      / \ / \
+#     0  4 7  9   
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(8)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+
+
+# Finding the lowest common ancestor of nodes 2 and 8
+solution = Solution()
+lca = solution.lowestCommonAncestor(root, root.left, root.right)
+print(f"The lowest common ancestor of nodes 2 and 8 is: {lca.val}") 
