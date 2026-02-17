ad0bd61603e569d38177c73f514cc265fda16533
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-17 18:04:44 +0100

    fix lca

--- trees/lowest_common_ancestor/interative_approach.py
+++ trees/lowest_common_ancestor/interative_approach.py
@@ -8,9 +8,9 @@ class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         cur = root
         while cur:
-            if p.val > root.val and q.val > root.val:
+            if p.val > cur.val and q.val > cur.val:
                 cur = cur.right
-            elif p.val < root.val and q.val < root.val:
+            elif p.val < cur.val and q.val < cur.val:
                 cur = cur.left
             else:
                 return cur
