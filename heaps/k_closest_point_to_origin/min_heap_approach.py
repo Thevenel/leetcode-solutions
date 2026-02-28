08ab7a8f9c243b73baad5b6cf515b7ccd44fc675
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-02-28 20:00:48 +0100

    k closest points to origin

---
+++ heaps/k_closest_point_to_origin/min_heap_approach.py
@@ -0,0 +1,24 @@
+import heapq
+
+
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        
+        min_heap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            min_heap.append([dist, x, y])
+            
+        heapq.heapify(min_heap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(min_heap)
+            res.append([x, y])
+            k -= 1
+        return res
+
+# Example usage:
+points = [[1,3],[-2,2]]
+k = 1
+solution = Solution()
+print(solution.kClosest(points, k))  # Output: [[-2,2]]
