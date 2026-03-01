ceeb6e3f7bc5b62e3604c6f7889c176a6618a915
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-03-01 23:23:17 +0100

    kth largest element in an array quick select approach

--- heaps/kth_largest_element_in_an_array/sorting_approach.py
+++ heaps/kth_largest_element_in_an_array/sorting_approach.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     solution = Solution()
     print(solution.findKthLargest([3, 2, 1, 5, 6, 4], 2))  # Output: 5
     print(solution.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))  # Output: 4
-    
