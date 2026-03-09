007361e0c9bd7b9e380d24a34924125b407ba8b9
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-03-09 18:57:57 +0100

    permutations rec

---
+++ backtracking/permutations/recursive_approach.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        perms = self.permute(nums[1:])
+        
+        res = []
+        
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        return res
+    
+# Usage
+solution = Solution()
+print(solution.permute([1, 2, 3]))  # Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+print(solution.permute([0, 1]))  # Output: [[0, 1], [1, 0]]
+print(solution.permute([1]))  # Output: [[1]]
