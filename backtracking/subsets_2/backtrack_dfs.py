eebb93b681b3f7c70981ad282e18d1f1ea9ea42f
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-03-10 17:55:28 +0100

    subsets 2

---
+++ backtracking/subsets_2/backtrack_dfs.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        subset = []
+        
+        def dfs(i):
+            
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            
+            dfs(i + 1)
+        dfs(0)
+        
+        return res
+    
+# Usage Example
+
+solution = Solution()
+print(solution.subsetsWithDup([1, 2, 2]))
+# Output: [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]] 
