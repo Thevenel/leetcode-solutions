0afca5ee21dea3b8352b90acd9da25b980dbad65
Thevenel Joazard <47800812+Thevenel@users.noreply.github.com>
2026-03-14 00:00:57 +0100

    generate parentheses

---
+++ backtracking/generate_parentheses/backtrack_dfs.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        stack = []
+        res = []
+        
+        def backtrack(open_n, close_n):
+            
+            if open_n == close_n == n:
+                res.append("".join(stack))
+                return
+        
+            if open_n < n:
+                stack.append('(')
+                backtrack(open_n +1, close_n)
+                stack.pop()
+            
+            if close_n < open_n:
+                stack.append(")")
+                backtrack(open_n, close_n + 1)
+                stack.pop()
+            
+        backtrack(0, 0)
+        return res
+# Usage Example
+solution = Solution()
+print(solution.generateParenthesis(3))
+# Output: ["((()))","(()())","(())()","()(())","()()()"]    
